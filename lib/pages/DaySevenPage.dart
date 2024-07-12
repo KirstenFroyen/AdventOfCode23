488955da436c55794d8052aa6d0911391bcd9419
Kirsten Froyen <kirsten.froyen@hotmail.com>
2024-07-12 21:27:36 +0200

    Add day 7 part one

--- lib/pages/DaySevenPage.dart
+++ lib/pages/DaySevenPage.dart
@@ -2,6 +2,7 @@
 
 import 'package:advent_of_code_23/services/FileReader.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'dart:async';
 
 class DaySevenPage extends StatefulWidget {
@@ -15,6 +16,7 @@ class _DaySevenPageState extends State<DaySevenPage> {
 
   late Future<int> _partOne;
   late Future<int> _partTwo;
+  static const List<String> _playingCards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"];
 
   @override
   void initState() {
@@ -24,13 +26,61 @@ class _DaySevenPageState extends State<DaySevenPage> {
   }
 
   Future<int> _findPartOne() async {
-    List<String> lines = await FileReader.readFileLines("day7/testInput.txt");
-    return lines.length;
+    List<String> lines = await FileReader.readFileLines("day7/puzzleInput.txt");
+    return totalWinnings(lines);
   }
 
   Future<int> _findPartTwo() async {
     List<String> lines = await FileReader.readFileLines("day7/testInput.txt");
     return lines.length;
+  }
+
+  int totalWinnings(List<String> input) {
+    var hands = _parseHands(input);
+
+    hands.sort((a, b) => compareHands(a, b));
+
+    int totalWinnings = 0;
+
+    for (int i = 0; i < hands.length; i++) {
+      totalWinnings += hands[i].bid * (i + 1);
+    }
+
+    return totalWinnings;
+  }
+
+  List<Hand> _parseHands(List<String> input) {
+    List<Hand> hands = [];
+    for (String line in input) {
+      var splitLine = line.split(" ");
+      hands.add(Hand(splitLine[0].split(""), int.parse(splitLine[1])));
+    }
+
+    return hands;
+  }
+
+  int cardValue(String card) {
+    return 14 - _playingCards.indexOf(card);
+  }
+
+  List<int> cardValues(List<String> cards) {
+    return cards.map((card) => cardValue(card)).toList();
+  }
+
+  int compareHands(Hand a, Hand b) {
+    if (a.type != b.type) {
+      return a.type.index.compareTo(b.type.index);
+    } else {
+      var aValues = cardValues(a.cards);
+      var bValues = cardValues(b.cards);
+
+      for (int i = 0; i < aValues.length; i++) {
+        if (aValues[i] != bValues[i]) {
+          return aValues[i].compareTo(bValues[i]);
+        }
+      }
+    }
+    return 0;
   }
 
   @override
@@ -50,8 +100,9 @@ class _DaySevenPageState extends State<DaySevenPage> {
                 if (snapshot.hasError) {
                   return Text('Error: ${snapshot.error}');
                 } else {
+                  final formattedResult = NumberFormat.decimalPattern('nl').format(snapshot.data);
                   return Text(
-                    'Part one:\n${snapshot.data}',
+                    'Part one:\n$formattedResult',
                     style: const TextStyle(fontSize: 20),
                   );
                 }
@@ -79,4 +130,41 @@ class _DaySevenPageState extends State<DaySevenPage> {
       ),
     );
   }
+}
+
+class Hand {
+  final List<String> cards;
+  final int bid;
+
+  Hand(this.cards, this.bid);
+
+  HandType get type => _handType(cards);
+}
+
+enum HandType {
+  highCard,
+  onePair,
+  twoPair,
+  threeOfAKind,
+  fullHouse,
+  fourOfAKind,
+  fiveOfAKind,
+}
+
+HandType _handType(List<String> cards) {
+  var counts = <String, int>{};
+
+  for (var card in cards) {
+    counts[card] = (counts[card] ?? 0) + 1;
+  }
+
+  var countValues = counts.values.toList()..sort((a, b) => b - a);
+
+  if (countValues[0] == 5) return HandType.fiveOfAKind;
+  if (countValues[0] == 4) return HandType.fourOfAKind;
+  if (countValues[0] == 3 && countValues[1] == 2) return HandType.fullHouse;
+  if (countValues[0] == 3) return HandType.threeOfAKind;
+  if (countValues[0] == 2 && countValues[1] == 2) return HandType.twoPair;
+  if (countValues[0] == 2) return HandType.onePair;
+  return HandType.highCard;
 }
