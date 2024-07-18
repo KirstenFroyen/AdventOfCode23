c7004f927dc311451e1690fcfee4d5c0a72b439f
Kirsten Froyen <kirsten.froyen@hotmail.com>
2024-07-18 10:19:42 +0200

    Add day 10 part one

--- lib/pages/10/DayTenPage.dart
+++ lib/pages/10/DayTenPage.dart
@@ -25,13 +25,79 @@ class _DayTenPageState extends State<DayTenPage> {
   }
 
   Future<int> _findPartOne() async {
-    List<String> lines = await FileReader.readFileLines("day10/testInput.txt");
-    return lines.length;
+    List<String> lines = await FileReader.readFileLines("day10/puzzleInput.txt");
+    return calculateSteps(lines);
   }
 
   Future<int> _findPartTwo() async {
     List<String> lines = await FileReader.readFileLines("day10/testInput.txt");
     return lines.length;
+  }
+
+  int calculateSteps(List<String> input) {
+    List<String> grid = input;
+    Map<String, List<String>> graph = {};
+    Set<String> visited = {};
+    Set<String> q = {};
+
+    for (int x = 0; x < grid.length; x++) {
+      String line = grid[x].trim();
+
+      for (int y = 0; y < line.length; y++) {
+        String tile = line[y];
+        
+        if (tile != '.') {
+          List<String> adjacent = [];
+
+          if ('-J7S'.contains(tile)) {
+            adjacent.add("$x,${y-1}");
+          }
+          if ('-FLS'.contains(tile)) {
+            adjacent.add("$x,${y+1}");
+          }
+          if ('|F7S'.contains(tile)) {
+            adjacent.add("${x+1},$y");
+          }
+          if ('|LJS'.contains(tile)) {
+            adjacent.add("${x-1},$y");
+          }
+          if (tile == 'S') {
+            visited.add('$x,$y');
+            q.add('$x,$y');
+          }
+
+          graph['$x,$y'] = adjacent;
+        }
+      }
+    }
+
+    int steps = -1;
+
+    while (q.isNotEmpty) {
+      Set<String> nxt = {};
+
+      for (String pos in q) {
+
+        if (graph.containsKey(pos)) {
+          for (String adj in graph[pos]!) {
+            List<int> adjXY = adj.split(',').map(int.parse).toList();
+            int x2 = adjXY[0];
+            int y2 = adjXY[1];
+            String adjKey = '$x2,$y2';
+
+            if (!visited.contains(adjKey) && graph.containsKey(adjKey) && graph[adjKey]!.contains(pos)) {
+              nxt.add(adjKey);
+              visited.add(adjKey);
+            }
+          }
+        }
+      }
+
+      q = nxt;
+      steps++;
+    }
+
+    return steps;
   }
 
   @override
